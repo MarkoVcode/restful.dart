c3ea50f8a636f7d139a8aeded9cc0b284b292076
Marek Kaszewski <marek.kaszewski@gmail.com>
2014-08-01 19:09:37 +0100

    adding selection of resource types

--- lib/src/rest_api.dart
+++ lib/src/rest_api.dart
@@ -1,7 +1,6 @@
 library restful.rest_api;
 
 import 'package:restful/src/formats.dart';
-import 'package:restful/src/resources/resource.dart';
 import 'package:restful/src/resources/cached.dart';
 import 'package:restful/src/resources/noncached.dart';
 import 'package:restful/src/uri_helper.dart';
@@ -9,16 +8,17 @@ import 'package:restful/src/uri_helper.dart';
 class RestApi {
   final String apiUri;
   final Format format;
-  final bool cached;
   
-  RestApi(this.apiUri, this.format, this.cached);
+  RestApi(this.apiUri, this.format);
   
-  Resource resource(String name) {
+  CachedResource cachedResource(String name) {
     var uri = new UriHelper.from(apiUri).appendEach(name.split("/")).toString();
-    if(this.cached) {
-      return new CachedResource(uri, format);
-    } else {
-      return new NonCachedResource(uri, format);
-    }
+    return new CachedResource(uri, format);
   }
+  
+  NonCachedResource nonCachedResource(String name) {
+    var uri = new UriHelper.from(apiUri).appendEach(name.split("/")).toString();
+    return new NonCachedResource(uri, format);
+  }
+
 }
